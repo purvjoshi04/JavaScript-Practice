7d5bad535eb68f4aa0b9585018e93c8a009b44cc
Purv Joshi <101319136+purvjoshi04@users.noreply.github.com>
2023-12-21 16:32:46 +0000

    Javascript datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using nodejs not browser 
+
+//primitive datatypes
+let name = "User"; //string
+let age = 20; //number
+let isLoggedIn = true; //boolean
+let userTodaysPurchase = null; //null is stanalone value
+let userState; // value is not assign yet
+//symbol type => unique
+
+console.log(typeof null); // output: object
+console.log(typeof undefined); // output: undefined
+
+
+
