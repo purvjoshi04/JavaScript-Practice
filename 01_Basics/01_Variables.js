35f290918e5d7896d3b9724a4ca522cf2c6cd467
Purv Joshi <101319136+purvjoshi04@users.noreply.github.com>
2023-12-21 15:06:46 +0000

    javascript variable declarations and scopes basics

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,17 @@
+const accountId = 1234;
+let accountEmail = "user@gmail.com";
+var accountPassword = "user@123";
+
+let accountState;
+// accountId = 12345;
+accountEmail = "user123@gmail.com";
+accountPassword = "user@12345";
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+
+//In javascript we can not redefine const variable it compiler give assignment to constant variable error which means you can not redefine the variable which in const scope keep in mind
+
+// new console log output which i don't know about which is this:
+console.table([accountId ,accountEmail, accountPassword, accountState]);
+// this will log the value in tabular form which is very cool LOL :)
