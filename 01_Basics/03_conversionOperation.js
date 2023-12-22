a9ce6ff8ac4e2e1a2a70791ee8dd459eede62f0a
Purv Joshi <101319136+purvjoshi04@users.noreply.github.com>
2023-12-22 10:52:49 +0000

    Javascript operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -32,10 +32,47 @@
 // "" => false
 // "user" => true
 
-let num = 33
-let stringNum = String(num);
-console.log(stringNum);
-console.log(typeof stringNum);
+// let num = 33
+// let stringNum = String(num);
+// console.log(stringNum);
+// console.log(typeof stringNum);
 
 // 33 => 33 in string
 
+// ***************************************** Operations *****************************************
+
+let value = 4;
+let negetiveValue = -value
+// console.log(negetiveValue);
+
+// console.log(3+3);
+// console.log(3-3);
+// console.log(3*3);
+// console.log(3**4);
+// console.log(3/4);
+// console.log(3%4);
+
+let str1 = "hello";
+let str2 = " world";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(((3 + 4) * 5) % 3);
+
+// console.log(true);
+// console.log(1+true);
+// console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 1 // less readability
+
+let gameCounter = 50;
+++gameCounter
+gameCounter++
+console.log(gameCounter);
