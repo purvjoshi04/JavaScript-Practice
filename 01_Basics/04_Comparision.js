15cf031fd6432005a94ca9006dfafab536f04940
Purv Joshi <101319136+purvjoshi04@users.noreply.github.com>
2023-12-22 11:09:07 +0000

    Javascript comparision

---
+++ 01_Basics/04_Comparision.js
@@ -0,0 +1,28 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// ********* confusion comparision *************
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined >= 0);
+// console.log(undefined == 0);
+// ********* confusion comparision ************* 
+
+
+// in javacsript comparision and equility check are different meaning comparision check value is same or not same or equals
+// "===" this operator is referes to the strict check between two equility check means it also checks the variable or value datatypes
+// example:
+
+let str1 = "Hello World";
+let num = 10;
+console.log(str1 === num);
+// false
