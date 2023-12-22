1468cf6cba8f0c97b07e9ef3d8ec2aa039932e44
Purv Joshi <101319136+purvjoshi04@users.noreply.github.com>
2023-12-22 06:26:35 +0000

    Conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,41 @@
+// let score = "39asd";
+// console.log(typeof score);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+
+// let scoreTwo = null;
+// let valueInNum = Number(scoreTwo);
+// console.log(valueInNumber);
+// console.log(typeof valueInNum);
+
+// let scoreThree = undefined;
+// let valueInNumThree = Number(scoreThree)
+// console.log(typeof valueInNumThree);
+// console.log(valueInNumThree);
+
+// let scoreFour = true
+// let valueInNumFour = Number(scoreFour);
+// console.log(typeof valueInNumFour);
+// console.log(valueInNumFour);
+
+// "39" => 33
+// "39asv" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 3;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "user" => true
+
+let num = 33
+let stringNum = String(num);
+console.log(stringNum);
+console.log(typeof stringNum);
+
+// 33 => 33 in string
+
