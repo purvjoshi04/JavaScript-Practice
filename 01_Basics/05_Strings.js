3b48b78b80d1a9ef19edfe43547f4bdcb94dbf64
Purv Joshi <101319136+purvjoshi04@users.noreply.github.com>
2023-12-24 16:38:12 +0000

    Javascript string prototype methods

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,32 @@
+const name = "purv joshi";
+const repoCount = 13;
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// second way to declare string 
+
+const nametwo = new String('Purv-Joshi');
+// console.log(nametwo.length);
+// console.log(nametwo.toUpperCase());
+// console.log(nametwo.charAt(9));
+// console.log(nametwo.indexOf('v'));
+
+const newStr = nametwo.substring(0, 5)
+// console.log(newStr);
+
+const anotherString = nametwo.slice(-8, 4);
+// console.log(anotherString); 
+
+const newStrOne = "     Purv Joshi      ";
+// console.log(newStrOne);
+// console.log(newStrOne.trim())
+
+const url = "https://pj-vcard.vercel.app";
+// console.log(url.replace("-", "%20"));
+
+// console.log(url.includes("%20"));
+
+// console.log(nametwo.split("-"));
+
+
+
