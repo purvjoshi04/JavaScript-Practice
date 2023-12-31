526deffbb9d04f0c1fa292bd303d26909f1893e6
Purv Joshi <101319136+purvjoshi04@users.noreply.github.com>
2023-12-31 06:25:30 +0000

    math and num in javascript

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,34 @@
+const score = 450;
+// console.log(score);
+
+const balance = new Number(20304);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNum = 23.34123;
+// console.log(otherNum.toPrecision(3));
+
+const numThird = 1000000;
+// console.log(numThird.toLocaleString('en-IN'));
+
+// ****************** Maths ******************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(3.4));
+// console.log(Math.ceil(3.4));
+// console.log(Math.floor(3.4));
+// console.log(Math.min(34,2,431,4,0.1));
+// console.log(Math.max(34,2,431,4,0.1));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
